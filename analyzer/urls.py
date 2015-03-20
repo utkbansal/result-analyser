3bc982e17a367d9dc157fb106ff1f893dd2908e3
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2015-03-20 23:19:13 +0530

    Adds AnalysisView at /analysis
    * now the analysis page is on its own AnalysisView view
    * adds use of django-jsonify to convert unicode data to json

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls import patterns,  url
-from .views import AnalysisFormView, LoginView, LogOutView, PasswordChangeView
+from .views import (
+    AnalyserFormView,
+    LoginView,
+    LogOutView,
+    PasswordChangeView,
+    AnalysisView
+)
 
 urlpatterns = patterns(
     '',
-    url(r'^analyze/$', AnalysisFormView.as_view(), name='analyze'),
-    # url(r'^analysis/$', AnalysisView.as_view(), name='analysis'),
     url(r'^$', LoginView.as_view(), name='login'),
+    url(r'^analyze/$', AnalyserFormView.as_view(), name='analyze'),
+    url(r'^analysis/$', AnalysisView.as_view(), name='analysis'),
     url(r'^logout/$', LogOutView.as_view(), name='logout'),
     url(r'^reset/$', PasswordChangeView.as_view(), name='reset'),
 )
--- result_analyzer/settings.py
+++ result_analyzer/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'crispy_forms',
     'django.contrib.webdesign',
     'analyzer',
+    'jsonify',
 )
 
 MIDDLEWARE_CLASSES = (
